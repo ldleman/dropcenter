aad5530eb8113a35791fc2e62eaccadf086bd08b
Idleman <idleman@idleman.fr>
2013-08-25 14:06:00 +0200

    MAJ sécurité

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require_once('header.php') ;
 	$tpl->assign('userList',parseUsers('./'));
 	$tpl->assign('dir',scandir(DIR_LANG));
-	$tpl->assign('error',(isset($_GET['error'])?$_GET['error']:null));
+
+	$tpl->assign('error',(isset($_GET['error'])?addslashes(htmlentities($_GET['error'])):null));
 	$view = 'index';
 require_once('footer.php');
 ?>
